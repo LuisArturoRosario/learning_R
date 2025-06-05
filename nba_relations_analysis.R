3fed5584bac62e5caa3fd7e6e2f98b55638430cb
unknown <akoi@cpal.org>
2025-06-05 16:16:53 -0500

    Charted Player's PPG alongside games played

---
+++ nba_relations_analysis.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(tidyverse)
+
+nba_players <- read_csv("C:\\Users\\amariik\\Documents\\learning_R\\datasets\\NBA_PLAYERS.csv")
+
+ggplot(nba_players, aes(x = G, y = PTS)) +
+  geom_point(aes(size = PTS / 100, color = HOF, alpha = 0.6)) + 
+  scale_color_manual(values = c("TRUE" = "green", "FALSE" = "red")) + 
+  labs(title = "NBA Players PPG compared to Games played",
+       x = "Games",
+       y = "Points per game") +
+  coord_cartesian(xlim = c(400, 1000)) +
+  theme_minimal()
+
