cfecfb3888a9f0f5f34a03f8c64ed5f4a53b779d
Luis Arturo Rosario <luis@cpal.org>
2025-06-06 17:04:44 -0500

    fix formatting

--- scripts/world_map_analysis.R
+++ scripts/world_map_analysis.R
@@ -19,14 +19,14 @@ map_data <- read_csv("datasets/world_data_2023.csv")
 
 # Standardize country names in the map data
 map_data <- map_data %>%
-  mutate(Country_std = countrycode(Country, origin = 'country.name', destination = 'country.name'))
+  mutate(Country_std = countrycode(Country, origin = "country.name", destination = "country.name"))
 
 # Get a world map with geometries
 world <- ne_countries(scale = "medium", returnclass = "sf")
 
 # Again, standardize country names in the world map data
 world <- world %>%
-  mutate(name_std = countrycode(name, origin = 'country.name', destination = 'country.name'))
+  mutate(name_std = countrycode(name, origin = "country.name", destination = "country.name"))
 
 # Conserve all world data and join it with our map data set
 map_merged <- world %>%
@@ -42,10 +42,13 @@ ggplot(map_merged) +
   geom_sf(aes(fill = `Gasoline Price`)) +
   scale_fill_viridis_c(option = "plasma", na.value = "grey90", labels = label_dollar(prefix = "$", accuracy = 0.01)) +
   theme_minimal() +
-  labs(title = "Global Gasoline Prices (2023)", fill = "Gasoline Price (Per Liter) ", 
-       caption = caption_text) + theme(
-         plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
-         plot.caption = ggtext::element_markdown(hjust = 0.5, size = 8),
-       )
+  labs(
+    title = "Global Gasoline Prices (2023)", fill = "Gasoline Price (Per Liter) ",
+    caption = caption_text
+  ) +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+    plot.caption = ggtext::element_markdown(hjust = 0.5, size = 8),
+  )
 
 ggsave("plots/global_gasoline_prices_2023.png")
