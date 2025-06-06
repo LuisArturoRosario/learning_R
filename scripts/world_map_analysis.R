270a1e768c7851353441acd5bb245730393df407
Luis Arturo Rosario <luis@cpal.org>
2025-06-06 15:59:26 -0500

    :bug: fix world map analysis file

--- scripts/world_map_analysis.R
+++ scripts/world_map_analysis.R
@@ -2,26 +2,31 @@ library(tidyverse)
 library(ggplot2)
 library(sf)
 library(rnaturalearth)
-library(countrycode))
+library(countrycode)
 library(rnaturalearthdata)
 
 map_data <- read_csv("datasets/world_data_2023.csv")
 
+# Standardize country names in the map data
 map_data <- map_data %>%
-  mutate(Country_std = countrycode(Country, origin = 'country.name', destination = 'country.name')
+  mutate(Country_std = countrycode(Country, origin = 'country.name', destination = 'country.name'))
 
 # Get a world map with geometries
 world <- ne_countries(scale = "medium", returnclass = "sf")
 
-# Join your gasoline data by country name
-map_merged <- world %>%
-  left_join(world_map_data, by = c("name" = "Country"))
+# Again, standardize country names in the world map data
+world <- world %>%
+  mutate(name_std = countrycode(name, origin = 'country.name', destination = 'country.name'))
 
+# Conserve all world data and join it with our map data set
+map_merged <- world %>%
+  left_join(map_data, by = c("name_std" = "Country_std"))
+
+# Convert the Gasoline Price to numeric after removing the dollar sign
 map_merged$`Gasoline Price` <- as.numeric(gsub("[$]", "", map_merged$`Gasoline Price`))
 
-# Plot it
 ggplot(map_merged) +
   geom_sf(aes(fill = `Gasoline Price`)) +
   scale_fill_viridis_c(option = "plasma", na.value = "grey90") +
   theme_minimal() +
-  labs(title = "Global Gasoline Prices", fill = "Gasoline Price")
+  labs(title = "Global Gasoline Prices (2023)", fill = "Gasoline Price")
