bb02e58490c739c836aed77556ee516bc5f8a9f9
CJ Lester <craig@cpal.org>
2025-06-06 16:58:22 -0500

    Standardize the regions across map_data and covid_data

--- scripts/covid_19_analysis.R
+++ scripts/covid_19_analysis.R
@@ -1,15 +1,30 @@
 library(tidyverse)
 library(ggplot2)
 library(maps)
+library(countrycode)
 
 covid_data <- read_csv("datasets/country_wise_latest.csv")
 
-world_map <- map_data("world") %>% 
-    left_join(covid_data, by = "region")
+covid_data <- covid_data %>%
+  mutate(Standardized_region = countrycode(
+    Country,
+    origin = "country.name",
+    destination = "country.name"
+  ))
 
-valid_data <- world_map %>% 
-    filter(!is.na(Confirmed) & !is.na(Deaths) & !is.na(Recovered) & !is.na(Active))
+map_data <- map_data("world") %>%
+  mutate(Standardized_region = countrycode(
+    region,
+    origin = "country.name",
+    destination = "country.name"
+  ))
+
+world_map <- map_data %>%
+  left_join(covid_data, by = "Standardized_region")
+
+valid_data <- world_map %>%
+  filter(!is.na(Confirmed) & !is.na(Deaths) & !is.na(Recovered) & !is.na(Active))
 
 ggplot(valid_data, aes(x = long, y = lat, group = group, fill = Confirmed)) +
-    geom_polygon(aes(fill = Confirmed), color = "black") +
-    labs(title = "Confirmed Cases by Country", fill = "Confirmed")
+  geom_polygon(aes(fill = Confirmed), color = "black") +
+  labs(title = "Confirmed Cases by Country", fill = "Confirmed")
