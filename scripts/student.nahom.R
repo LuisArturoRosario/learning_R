e6cc35feaa8489271175ccb64c531a9cbab13356
CJ Lester <craig@cpal.org>
2025-06-10 16:56:52 -0500

    Add plot variables to original analysis files

--- scripts/student.nahom.R
+++ scripts/student.nahom.R
@@ -1,11 +1,13 @@
 library(ggplot2)
 library(tidyverse)
 
+data <- read_csv("C:\\Users\\clester\\Documents\\Code Projects\\learning_R\\datasets\\student_habits_performance.csv")
+
 new_score <-data %>%
   group_by(social_media_hours) %>%
   summarise(exam_scores = mean(exam_score))
 
-ggplot(new_score, aes(x = social_media_hours, y = exam_scores)) +
+social_media_plot <- ggplot(new_score, aes(x = social_media_hours, y = exam_scores)) +
   geom_col() +
   geom_smooth(method = "lm", se = FALSE) +
   labs(
@@ -15,3 +17,4 @@ ggplot(new_score, aes(x = social_media_hours, y = exam_scores)) +
   ) + 
   coord_cartesian(ylim = c(0, 100))
 
+social_media_plot
