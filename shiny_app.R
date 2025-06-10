9b209e5c3961e03dc4ba89b68b4b8076cdd1bd32
Luis Arturo Rosario <luis@cpal.org>
2025-06-10 16:10:56 -0500

    add data table
    Co-authored-by: Craig (CJ) Lester <CJLester@users.noreply.github.com>

--- shiny_app.R
+++ shiny_app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(bslib)
 library(ggplot2)
+library(DT)
 
 source("scripts/ai_job_analysis.R")
 
@@ -8,18 +9,23 @@ ui <- fluidPage(
   tags$head(
     tags$style(HTML("
     .control-example {
-      text-align: center !important;
+      text-align: center;
+      font-weight: bold
       }
+    .dt-buttons .btn {
+      background-color: #007bff;
+      color: black;
+      border: none;
+      padding: 8px 12px;
+      margin: 2px;
+      border-radius: 4px;
+    }
+    .dt-buttons .btn:hover {
+      background-color: #0056b3;
+    }
     "))
   ),
   titlePanel(HTML("<b>Dashboard Template</b>")),
-  div(
-    HTML(
-      "Credits to all dataset authors. They will be linked in the paper."
-    ),
-    style = "position: absolute; bottom: 0;
-    left: 0; right: 0; text-align: center;"
-  ),
   page_sidebar(
     sidebar = sidebar(
       h1(
@@ -29,32 +35,44 @@ ui <- fluidPage(
         style = "color: blue;"
       ),
       p("You can put any controls here, like sliders, inputs, etc."),
-      sliderInput("slider_example",
-        label = div(HTML("<b>slider example</b>"), class = "control-example"),
-        min = 0,
-        max = 100,
-        value = 50
-      ),
-      selectInput(
-        "select_example",
-        div(HTML("Select Value:"), class = "control_example"),
-        list("Value 1" = 1, "Value 2" = 2, "Value 3" = 3)
+      div(
+        sliderInput("slider_example",
+          label = HTML("<b>slider example</b>"),
+          min = 0,
+          max = 100,
+          value = 50,
+        ),
+        selectInput(
+          "select_example",
+          HTML("Select Value:"),
+          list("Value 1" = 1, "Value 2" = 2, "Value 3" = 3),
+        ),
+        class = "control-example"
       ),
       input_switch("switch_example", textOutput("switch_state"))
     ),
-  mainPanel(
-    h1("This is the main area", style = "text-align: center;"),
-    plotOutput("new_plot")
-  ),
-  absolutePanel(
-    wellPanel(
-      titlePanel(HTML("<b>Did you know?</b>")),
-      HTML("This website has <b>NO</b> <em>data?</em>")
+    mainPanel(
+      h1("This is the main area", style = "text-align: center;"),
+      plotOutput("new_plot"),
+      hr(),
+      h3("Explore Data Table"),
+      DTOutput("cool_table"),
+      p("Tip: Use the search box to filter results, or click column headers to sort.")
     ),
-    width = "200px",
-    right = "50px",
-    top = "20px",
-    draggable = TRUE
+    absolutePanel(
+      wellPanel(
+        titlePanel(HTML("<b>Did you know?</b>")),
+        HTML("This website has <b>NO</b> <em>data?</em>")
+      ),
+      width = "200px",
+      right = "50px",
+      top = "20px",
+      draggable = TRUE
+    ),
+    div(
+      HTML("Credits to all dataset authors. They will be linked in the paper."),
+      style = "text-align: center; margin-top: 50px; padding: 20px;
+        border-top: 1px solid #ddd; font-style: italic;"
     )
   )
 )
@@ -79,6 +97,19 @@ server <- function(input, output) {
     ai_plot
   })
 
+  output$cool_table <- DT::renderDataTable({
+    DT::datatable(
+      ai_jobs,
+      options = list(
+        pageLength = 10,
+        autoWidth = TRUE,
+        scrollX = TRUE,
+        dom = "Bfrtip",
+        buttons = c("copy", "csv", "excel", "pdf", "print")
+      ),
+      extensions = "Buttons"
+    )
+  })
 
   output$switch_state <- renderText({
     switch_state_msg()
